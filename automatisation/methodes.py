86628f108cc3353df04b26375cd738de7025aa4a
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-05-01 12:54:30 +0100

    optimizing methods implementation

--- automatisation/methodes.py
+++ automatisation/methodes.py
@@ -1,168 +1,129 @@
 import re
-from .constantes import *
 import os
 import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from winotify import Notification, audio
 from .LoginPage import tkinter_page
+from .constantes import *
 
-
-class Methodes :
-    def __init__(self) :
-         self.dictionnaire_first = not os.path.isfile("data.json")
-         self.page = None
-         self.cours_consultés = {}
-         self.user_name = None
-         self.password = None
-         self.telecharger = "y"
-         self.First = False
-          
-        # instancier l'objet toast pour afficher les notifications
-         self.Toast = Notification(app_id = "Moodle",
-            title = "Attention",
-            msg = "un cours a été ajouté sur moodle",
-            duration = "long",
-            icon = r"C:\Users\ayoub\Desktop\moodle-automate\automatisation\Mobile-M-Icon-1-corners.png")
-         self.Toast.set_audio(audio.Mail, loop = False)
-
-
-
+class Methodes:
+    def __init__(self):
+        self.First = False
+        self.page = None
+        self.user_name = None
+        self.password = None
+        self.telecharger = "y"
+        self.cours_consultes = {}
+        self.toast = Notification(
+            app_id="Moodle",
+            title="Attention",
+            msg="Un cours a été ajouté sur Moodle",
+            duration="long",
+            icon=r"C:\Users\ayoub\Desktop\moodle-automate\automatisation\Mobile-M-Icon-1-corners.png"
+        )
+        self.toast.set_audio(audio.Mail, loop=False)
 
     def est_premiere_fois(self):
-        """ cette fonction permet de vérifier si le programme se lance pour la première fois
-        si c'est le cas elle crée un fichier qui contient le mot de passe 
-        et le nom de l'utilisateur et les cours qu'il a vus sur moodle"""
-        if  not os.path.isfile(login_path):
+        if not os.path.isfile(login_path):
             tkinter_page0 = tkinter_page()
-            self.user_name , self.password = tkinter_page0.user_name , tkinter_page0.password
+            self.user_name, self.password = tkinter_page0.user_name, tkinter_page0.password
             self.telecharger = tkinter_page0.telecharger
-            with open(login_path,"w") as f :
-                f.write(self.user_name+"\n")
-                f.write(self.password)
-                f.close()
-            os.system(f"attrib +h {login_path}")
-            self.First = True
-        else : 
-            with open(login_path,"r") as f :
-                self.user_name = f.readline()
-                self.password = f.readline()
-                f.close()
-            if self.dictionnaire_first :
-                print("il semble que vous avez supprimé le fichier data.json \n")
-                self.telecharger = input("vous voulez télécharger les anciens cours ? [y/n] : ").lower()
-                while(self.telecharger not in {"y" , "n"}):
-                    self.telecharger = input("veuillez répondre par 'y' ou 'n' s'il vous plaît : ").lower()
-            else :
-                    json_file = open("data.json","r")
-                    self.cours_consultés = json.load(json_file)
-                    json_file.close()
+            with open(login_path, "w") as f:
+                f.write(f"{self.user_name}\n{self.password}")
+                os.system(f"attrib +h {login_path}")
+                self.First = True
+        else:
+            with open(login_path, "r") as f:
+                self.user_name, self.password = f.readline().strip(), f.readline().strip()
+                if self.dictionnaire_first:
+                    print("Il semble que vous ayez supprimé le fichier data.json.\n")
+                    self.telecharger = input("Voulez-vous télécharger les anciens cours ? [y/n] : ").lower()
+                    while self.telecharger not in {"y", "n"}:
+                        self.telecharger = input("Veuillez répondre par 'y' ou 'n' s'il vous plaît : ").lower()
+                else:
+                    with open("data.json", "r") as json_file:
+                        self.cours_consultes = json.load(json_file)
 
-
-    def load_and_login(self , browser):
+    def load_and_login(self, browser):
         self.page = browser.new_page()
-        self.page.goto(moodle , timeout=None)
-        self.page.locator("#username").fill(self.user_name)   
-        self.page.locator("#password").fill(self.password) 
+        self.page.goto(moodle, timeout=None)
+        self.page.locator("#username").fill(self.user_name)
+        self.page.locator("#password").fill(self.password)
         self.page.locator("#loginbtn").click()
 
-
-
-    def type_file(self , file , name):             # savoir si le document pdf s'agit d'un cours, TD ou bien TP
+    def type_file(self, name):
         if td.search(name):
             return "td"
-        elif tp.search(name) :
+        elif tp.search(name):
             return "tp"
         return "cours"
 
-
-    def lien_valide(self , lien):
-        to_replace = {"user":"course","1860&course=":"" , "&showallcourses=1":""}
-        for clé,valeur in to_replace.items() : 
-            lien = re.sub(clé , valeur , lien)
+    def lien_valide(self, lien):
+        to_replace = {"user": "course", "1860&course=": "", "&showallcourses=1": ""}
+        for cle, valeur in to_replace.items():
+            lien = re.sub(cle, valeur, lien)
         return lien
 
-
-    def modules_liens_liste(self):  
-        #cette fonction retourne la liste de liens des modules
+    def modules_liens_liste(self):
         self.page.goto(profile)
         modules = self.page.locator('.contentnode li a')
-        nombre_modules = modules.count()
-        liens_incomplets = [modules.nth(i).get_attribute("href") for i in range(nombre_modules) ]
-        return [*map(self.lien_valide, liens_incomplets )]
+        return [self.lien_valide(module.get_attribute("href")) for module in modules]
 
-    def deja_consulté(self , cours, module_name):
-        # pour vérifier si un cours est déjâ consulté
-        return cours in self.cours_consultés[module_name] 
+    def deja_consulte(self, cours, module_name):
+        return cours in self.cours_consultes.get(module_name, [])
 
-
-
-    def Telecharger_fichier(self,element, name , module_name , link) :          # element désigne un fichier 
-        ftype = self.type_file(element , name)
-        nom_sb = re.sub(r"\n[a-zA-Z]+|\\*\"*\/*\:*\**\?*\<*\>*\|*","",name)                   # pour supprimer "\nFichier" qui existe à la fin du nom
-        # parfois le fichier n'est pas un pdf il peut être une image par exemple
-        with self.page.expect_download() as download_info:
-            # initialiser le téléchargement
-                element.locator("css=.instancename").click()
-                # attendre que le téléchargement commence
-                download = download_info.value
-                # attendre la fin du processus du téléchargement
-                path = download.path()
-                # sauvegarder le fichier installé
-                download.save_as(f"{homedir}\Desktop\Moodle\{module_name}\{ftype}\{nom_sb}.pdf")
-                if not self.First :
-                    self.Toast.add_actions(label = "appuyez ici" , launch= f"{homedir}\Desktop\Moodle\{module_name}\{ftype}\{nom_sb}.pdf" )
-                    self.Toast.show()
-
-
-
-    def Telecharger_dossier(self , element , name , module_name , link) :
+    def telecharger_fichier(self, element, name, module_name):
+        ftype = self.type_file(name)
+        nom_sb = re.sub(r"\n[a-zA-Z]+|\\*\"*\/*\:*\**\?*\<*\>*\|*", "", name)  # Remove unnecessary characters
         element.locator("css=.instancename").click()
-        nom_sb = nom_sb = re.sub(r"\n[a-zA-Z]+|\\*\"*\/*\:*\**\?*\<*\>*\|*","",name)   # pour supprimer "\nDossier" qui existe à la fin du nom
-        download_button = self.page.locator('#region-main input').nth(0)
-
-        if download_button.get_attribute("type") == "hidden" :            # tester si le dossier est vide (y a pas de download button)
-                self.page.goto(link)
-                return 0
+        download_button = self.page.wait_for_selector('#region-main input[type="hidden"]', timeout=5)
+        if not download_button:
+            self.page.goto(self.page.url)
+            return
+        download_button.click()
         with self.page.expect_download() as download_info:
-        # initialiser le téléchargement
-            download_button.click()
-            # attendre que le téléchargement commence
             download = download_info.value
-            # attendre la fin du processus du téléchargement
             path = download.path()
-            # sauvegarder le fichier installer
-            download.save_as(f"{homedir}\Desktop\Moodle\{module_name}\{nom_sb}.zip")
-            self.page.goto(link)
-            if not self.First :
-                self.Toast.add_actions(label = "appuyez ici" , launch= f"{homedir}\Desktop\Moodle\{module_name}\{nom_sb}.zip")
-                self.Toast.show()
+            download.save_as(f"{homedir}\\Desktop\\Moodle\\{module_name}\\{ftype}\\{nom_sb}.pdf")
+            if not self.First:
+                self.toast.add_actions(label="Appuyez ici", launch=f"{homedir}\\Desktop\\Moodle\\{module_name}\\{ftype}\\{nom_sb}.pdf")
+                self.toast.show()
 
+    def telecharger_dossier(self, element, name, module_name):
+        element.locator("css=.instancename").click()
+        download_button = self.page.wait_for_selector('#region-main input[type="hidden"]', timeout=5)
+        if not download_button:
+            self.page.goto(self.page.url)
+            return
+        download_button.click()
+        nom_sb = re.sub(r"\n[a-zA-Z]+|\\*\"*\/*\:*\**\?*\<*\>*\|*", "", name)  # Remove unnecessary characters
+        with self.page.expect_download() as download_info:
+            download = download_info.value
+            path = download.path()
+            download.save_as(f"{homedir}\\Desktop\\Moodle\\{module_name}\\{nom_sb}.zip")
+            if not self.First:
+                self.toast.add_actions(label="Appuyez ici", launch=f"{homedir}\\Desktop\\Moodle\\{module_name}\\{nom_sb}.zip")
+                self.toast.show()
 
-
-
-    def elements_liste(self , link):
+    def elements_liste(self, link):
         self.page.goto(link)
         module_name = self.page.locator("css=h1").inner_text()
-        # chercher les fichier
         elements = self.page.locator("css=.modtype_resource , .modtype_folder ")
-        n = elements.count()
-        if self.dictionnaire_first :
-            self.cours_consultés[module_name] = []
-        for i in range (n):
-            cours_id = elements.nth(i).get_attribute("id")
-            est_pdf = elements.nth(i).locator(".activityicon").get_attribute("src")
-            if not self.deja_consulté(cours_id , module_name) :
-                if self.telecharger=="y" :
-                    name = (elements.nth(i)).locator("css=.instancename").inner_text()
-                    class_ = elements.nth(i).get_attribute("class")
-                    if class_.__contains__("modtype_resource") and est_pdf == pdf :
-                        self.Telecharger_fichier(elements.nth(i), name , module_name , link)
-                    elif  class_.__contains__("modtype_folder") :                                                      
-                        self.Telecharger_dossier(elements.nth(i), name , module_name , link)
-                self.cours_consultés[module_name].append(cours_id)
-    
+        for element in elements:
+            cours_id = element.get_attribute("id")
+            est_pdf = element.locator(".activityicon").get_attribute("src")
+            if not self.deja_consulte(cours_id, module_name):
+                if self.telecharger == "y":
+                    name = element.locator("css=.instancename").inner_text()
+                    if "modtype_resource" in element.get_attribute("class") and est_pdf == pdf:
+                        self.telecharger_fichier(element, name, module_name)
+                    elif "modtype_folder" in element.get_attribute("class"):
+                        self.telecharger_dossier(element, name, module_name)
+                self.cours_consultes.setdefault(module_name, []).append(cours_id)
+
     def finir_leprogramme(self):
-        json_file = open("data.json","w")
-        json.dump(self.cours_consultés , json_file)
-        json_file.close()
-
-
+        with open("data.json", "w") as json_file:
+            json.dump(self.cours_consultes, json_file)
